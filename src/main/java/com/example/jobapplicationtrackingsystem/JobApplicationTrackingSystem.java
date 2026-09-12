06d69220fbc9c0d659037fa5224683e2b31b0ce5
Dheeraj Chaudhary <dheerajchaudhary231@gmail.com>
2026-09-12 16:48:22 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/jobapplicationtrackingsystem/JobApplicationTrackingSystem.java
@@ -0,0 +1,13 @@
+package com.example.jobapplicationtrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackingSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationTrackingSystem.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobapplicationtrackingsystem/JobApplicationTrackingSystemTests.java
@@ -0,0 +1,13 @@
+package com.example.jobapplicationtrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackingSystemTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
